2f6784f6db6a8e467519b664b32f37bbbacd0ec5
mnieto-m <majanoyieto@gmail.com>
2026-01-14 21:59:52 +0100

    lest pr

--- src/envp_init.c
+++ src/envp_init.c
@@ -6,26 +6,31 @@
 /*   By: mnieto-m <mnieto-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:34:26 by mario             #+#    #+#             */
-/*   Updated: 2026/01/13 21:26:18 by mnieto-m         ###   ########.fr       */
+/*   Updated: 2026/01/14 21:59:03 by mnieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int free_list(t_list *env, t_env *aux)
-{
-	free_aux(aux);
-	if(env != NULL)
-		ft_lstclear(&env,free);
-	return(NULL);
-}
-int free_aux(t_env *aux)
+void free_aux(t_env *aux)
 {
 	if (!aux)
 		return;
 	free(aux->key);
 	free(aux->value);
 	free(aux);
+}
+void free_list(t_list **env)
+{
+	t_list *tmp;
+
+	while (*env)
+	{
+		tmp = (*env)->next;
+		free_aux((*env)->content);
+		free(*env);
+		*env = tmp;
+	}
 }
 t_env *new_value_env(char *str_key, char*str_value)
 {
@@ -34,11 +39,9 @@ t_env *new_value_env(char *str_key, char*str_value)
 	env = malloc(sizeof(*env));
 	if (!env)
 		return NULL;
-
 	env->key = ft_strdup(str_key);
 	if (!env->key)
 		return (free(env), NULL);
-
 	env->value = ft_strdup(str_value);
 	if (!env->value)
 		return (free(env->key), free(env), NULL);
@@ -53,6 +56,8 @@ void	update_shlvl(t_env *env)
 
 	lvl = ft_atoi(env->value);
 	new = ft_itoa(lvl + 1);
+	if(!new)
+		return;
 	free(env->value);
 	env->value = new;
 }
@@ -61,45 +66,39 @@ t_env *init_node(char *envp)
 	t_env	*env;
 	char	*aux;
 
-	aux = ft_strchr(envp, '=');
-	if (!aux)
-		return NULL;
-
 	env = malloc(sizeof(*env));
 	if (!env)
 		return NULL;
-
+	aux = ft_strchr(envp, '=');
 	env->key = ft_substr(envp, 0, aux - envp);
 	if (!env->key)
 		return (free(env), NULL);
-
 	env->value = ft_strdup(aux + 1);
 	if (!env->value)
 		return (free(env->key), free(env), NULL);
-
-	return env;
+	return (env);
 }
 
 t_list *init_envp(char **envp)
 {
-	t_list *env;
 	t_env *aux;
+	t_list *env;
+	t_list	*node;
 	int i;
 	
-	i = 0;
+	i = -1;
 	env = NULL;
-	aux = malloc(sizeof(*aux));
-	while(envp[i++])
+	while(envp[++i])
 	{
 		aux = init_node(envp[i]);
 		if(!aux)
-			free_aux(aux);
+			return (free_list(&env), NULL);
 		if(ft_strncmp(aux->key,"SHLVL",5) == 0)
-			update_shlvl(aux->value);
-		ft_lstadd_back(&env,ft_lstnew((void *)new_value_env(aux->key,aux->value)));
-		if(!env)
-			free_list(env, aux);// esto no es del todo correcto no se libera aux keu aux value and etc
-		free_aux(aux);// modificar en otra funcion
+			update_shlvl(aux);
+		node = ft_lstnew(aux);
+		if (!node)
+			return (free_aux(aux), free_list(&env), NULL);
+		ft_lstadd_back(&env,node);
 	}
 	return(env);
 }
